a083a463e6aa4d8636f7ed3d0ca78aeb89309bb2
Abdullah Huawei LT <aaydogan@gmail.com>
2022-12-18 14:30:47 +0300

    devcon

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,44 @@
 let gulp = require('gulp');
 let sass = require('gulp-sass')(require('sass'));
+let autoprefixer = require('gulp-autoprefixer');
+let csso = require('gulp-csso');
 let rename = require('gulp-rename');
+let concat = require('gulp-concat');
+let uglify = require('gulp-uglify');
 let browserSync = require('browser-sync').create();
 
 //SASS
 gulp.task('sass', function () {
     return gulp.src('./scss/style.scss')
         .pipe(sass())
-        .pipe(rename('otm_bs5.2.css'))
-        .pipe(gulp.dest('app/css'))
+        .pipe(autoprefixer({browserlist: [">= 1%", "last 1 major version", "not dead", "Chrome >= 60", "Firefox >= 60", "Edge >= 16", "iOS >= 10", "Safari >= 10", "Android >= 6", "not Explorer <= 11"]}))
+        .pipe(rename('otm_bs52.css'))
+        .pipe(gulp.dest('app/assets'))
+        .pipe(csso({comments:false}))
+        .pipe(rename('otm_bs52.min.css'))
+        .pipe(gulp.dest('app/assets'))
+});
+
+//JS
+gulp.task('js', function () {
+    return gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/@popperjs/core/dist/umd/popper.js', 'node_modules/bootstrap/dist/js/bootstrap.js'])
+        .pipe(concat('otm_bs52.js'))
+        .pipe(gulp.dest("app/assets"))
+        .pipe(uglify())
+        .pipe(rename('otm_bs52.min.js'))
+        .pipe(gulp.dest("app/assets"))
+        //.pipe(gulp.dest("../otmnew/o/js"))
+        //.pipe(browserSync.reload({
+            //stream: true
+        //}))
+});
+
+//FA
+gulp.task('fa', function () {
+    return gulp.src('node_modules/@fortawesome/fontawesome-free/webfonts/!(*brands*)')
+        .pipe(gulp.dest("app/fonts"))
+        //.pipe(gulp.dest("../otmnew/o/fonts"))
+        //.pipe(browserSync.reload({
+            //stream: true
+        //}))
 });
